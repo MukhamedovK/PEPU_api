907d266673ed20749c00669b717d268414d3c7bf
MukhamedovK <kamoliddin2619@gmail.com>
2024-06-20 17:24:08 +0500

    added to admin.py ProductPhoto

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Size, Product, Category
+from .models import Size, Product, Category, ProductPhoto
 
 
 admin.site.register(Size)
 admin.site.register(Category)
+admin.site.register(ProductPhoto)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
